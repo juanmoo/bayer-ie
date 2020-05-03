a3e822bed8c0aea97eca29acab6142949fb0da1f
Juan M Ortiz <juanmoo@me.com>
2020-05-03 14:41:39 -0400

    Move matchig to utils

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ Utility functions to be used in Bayer Project
 
 import pandas as pd
 import os, sys, json, pickle
+from fuzzywuzzy import fuzz
 
 def parse_spreadsheet(path):
     '''
@@ -47,6 +48,60 @@ def parse_spreadsheet(path):
 
 
     return output
+
+# Match raw JSON-styled data to annotations
+
+def contains_test(piece, whole, exact_match=False):
+    # Fuzzy matching tests to see if the max
+    # similarity substring of size len(piece)
+    # has a partial ratio > threshold
+    if exact_match:
+        return piece in whole
+
+    threshold = 95
+    return fuzz.partial_ratio(piece, whole) >= threshold
+
+def match_labels(raw_data, annotations, exact_match=False):
+    labeled_raw_documents = {}
+
+    for parsed_doc_name in raw_data:
+        parsed_doc = raw_data[parsed_doc_name]
+        label_doc = annotations[parsed_doc_name] #label doc w/ same name
+        
+        matchings = [] # el: [parsed paragraph, label_paragraph_id, label]
+        other = []
+        
+        paragraphs = []
+        labels = []
+        tags = []
+        
+        for parsed_p, tag in zip(parsed_doc['element_text'], parsed_doc['element_tag']):
+            found = False
+            for i, label_p in enumerate(label_doc['texts']):
+                if contains_test(parsed_p, label_p, exact_match=exact_match):
+                    found = True
+                    matchings.append([parsed_p, i, label_doc['labels'][i]])
+                    paragraphs.append(parsed_p)
+                    labels.append(label_doc['labels'][i])
+                    tags.append(tag)
+                    break
+            if not found:
+                other.append(parsed_p)
+                
+                paragraphs.append(parsed_p)
+                labels.append('other')
+                tags.append(tag)
+                
+        
+        labeled_raw_documents[parsed_doc_name] = {
+            'matches': matchings,
+            'other': other,
+            'paragraphs': paragraphs,
+            'labels': labels,
+            'tags': tags
+        }       
+    
+    return labeled_raw_documents
 
 # Save / Load values from checkpoint file
 def save_value(key, val, path=None):
